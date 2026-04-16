f97969709565a2810bbdac0efd117267db1b6d0e
ONEBOYDJ <iamobdj@gmail.com>
2026-04-17 03:46:37 +0500

    fix all CAN defines for kernel 3.4

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# Фикс: добавить недостающие CAN define для ядра 3.4
-IPLINK="padavan-ng/trunk/user/busybox/busybox-1.37.0/networking/libiproute/iplink.c"
-if [ -f "$IPLINK" ]; then
-  sed -i '1i\
-#ifndef CAN_CTRLMODE_FD\n#define CAN_CTRLMODE_FD 0x04\n#endif\n#ifndef CAN_CTRLMODE_FD_NON_ISO\n#define CAN_CTRLMODE_FD_NON_ISO 0x08\n#endif\n#ifndef CAN_CTRLMODE_PRESUME_ACK\n#define CAN_CTRLMODE_PRESUME_ACK 0x40\n#endif' "$IPLINK"
-fi
+# Фикс: патч iplink.c - убрать CAN support (несовместим с ядром 3.4)
+cd padavan-ng/trunk
+find . -name "iplink.c" -path "*/busybox*" | while read f; do
+  sed -i '/do_set_can/,/^}/{ s/CAN_CTRLMODE_FD_NON_ISO/0x08/g; s/CAN_CTRLMODE_FD/0x04/g; s/CAN_CTRLMODE_PRESUME_ACK/0x40/g; s/IFLA_CAN_TERMINATION/999/g; }' "$f"
+done
+
+# Универсальный фикс: добавить все недостающие define в заголовок
+find . -name "iplink.c" -path "*/busybox*" | while read f; do
+  sed -i '1i #ifndef CAN_CTRLMODE_FD\n#define CAN_CTRLMODE_FD 0x04\n#endif\n#ifndef CAN_CTRLMODE_FD_NON_ISO\n#define CAN_CTRLMODE_FD_NON_ISO 0x08\n#endif\n#ifndef CAN_CTRLMODE_PRESUME_ACK\n#define CAN_CTRLMODE_PRESUME_ACK 0x40\n#endif\n#ifndef IFLA_CAN_TERMINATION\n#define IFLA_CAN_TERMINATION 22\n#endif' "$f"
+done
+cd ../..
